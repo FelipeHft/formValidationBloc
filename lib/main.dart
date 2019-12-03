0206d4e84920f0dc56c0674c54e0c9b42ad64325
pipeworkout <felipe.hernandezc@utem.cl>
2019-12-03 13:18:49 -0300

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:formvalidationbloc/src/bloc/provider.dart';
+import 'package:formvalidationbloc/src/pages/home_page.dart';
+import 'package:formvalidationbloc/src/pages/login_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Material App',
+        initialRoute: 'home',
+        routes: {
+          'login' : (BuildContext context) => LoginPage(),
+          'home'  : (BuildContext context) => HomePage(),
+        },
+        theme: ThemeData(
+          primaryColor: Colors.deepPurple
+        ),
+      ),
+    );
+  }
+}
